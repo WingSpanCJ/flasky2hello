e868b47703f390bc85107391c149ff55e58d613b
WingSpan <425357138@qq.com>
2017-08-31 02:27:25 +0800

    add  /test/<int:num>

--- flasky2hello.py
+++ flasky2hello.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/test/<int:num>')
+def numReceive(num:int):
+    import datetime
+    time = datetime.datetime.now()
+    return "{}: Number is {}.".format(time.strftime("%Y-%m-%d %H:%M:%S"), num)
 
 if __name__ == '__main__':
     app.run()
